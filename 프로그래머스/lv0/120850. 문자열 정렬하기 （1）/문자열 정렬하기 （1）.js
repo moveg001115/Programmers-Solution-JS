643047fa5bf5eb1313088f853ea37b0654f4ae50
moveg001115 <136950328+moveg001115@users.noreply.github.com>
2023-08-28 13:46:31 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let answer = [];
+    for(let i = 0; i < my_string.length; i++) {
+        if(isNaN(my_string[i])) continue;
+        else answer.push(Number(my_string[i]));
+    }
+    answer.sort((a, b) => a - b);
+    return answer;
+}
