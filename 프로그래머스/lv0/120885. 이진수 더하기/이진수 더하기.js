d4b7309b51efe14c7bfca1f1f0243f6860000379
moveg001115 <136950328+moveg001115@users.noreply.github.com>
2023-08-31 14:31:39 +0900

    [level 0] Title: 이진수 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,5 +1,29 @@
-function solution(bin1, bin2) {
-    var answer = '';
-    answer = (parseInt(bin1, 2) + parseInt(bin2, 2)).toString(2);
-    return answer;
+function solution(_bin1, _bin2) {
+    const bin1 = _bin1.split("").reverse();
+    const bin2 = _bin2.split("").reverse();
+
+    const N1 = bin1.length;
+    const N2 = bin2.length;
+
+    let i = 0;
+    let j = 0;
+
+    let sum = 0;
+    let carry = 0;
+    let result = [];
+
+    while (i < N1 || j < N2 || carry > 0) {
+        const n1 = Number(bin1[i]) || 0;
+        const n2 = Number(bin2[j]) || 0;
+
+        let localSum = carry + n1 + n2;
+
+        result.push(localSum % 2);
+        carry = Math.floor(localSum / 2);
+
+        i += 1;
+        j += 1;
+    }
+
+    return result.reverse().join("");
 }
