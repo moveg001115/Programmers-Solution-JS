a7c465d93dbdef97068af57d63a7c02704bc6f47
moveg001115 <136950328+moveg001115@users.noreply.github.com>
2023-09-01 19:28:10 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,10 @@
+function solution(common) {
+    var answer = 0;
+    if(common[common.length - 1] - common[common.length - 2] === common[1] - common[0]) {
+        answer = common[common.length - 1] + (common[1] - common[0]);
+    }
+    else if(common[common.length - 1] / common[common.length - 2] === common[1] / common[0]) {
+        answer = common[common.length - 1] * (common[1] / common[0]);
+    }
+    return answer;
+}
