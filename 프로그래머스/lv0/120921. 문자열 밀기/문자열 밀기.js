d318b4d2f5caa4731a33a9f34f8b50a02d304449
moveg001115 <136950328+moveg001115@users.noreply.github.com>
2023-09-03 14:56:05 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,11 +1,17 @@
 function solution(A, B) {
     var answer = 0;
+    
     if(A === B) return 0;
+    
     A = A.split("");
     for(let i = 0; i < A.length; i++) {
-        A.unshift(A.pop());
-        answer++;
-        if(A.join("") === B) return answer;
+        if(A.join("") === B) {
+            break;
+        }
+        else {
+            A.unshift(A.pop());
+            answer++;
+        }
     }
     return answer === A.length ? -1 : answer;
 }
