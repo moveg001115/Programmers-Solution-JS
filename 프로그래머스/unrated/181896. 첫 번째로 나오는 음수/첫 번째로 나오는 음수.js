b79fecec7d518e878b10faa3d3d985775ad7fc0c
moveg001115 <136950328+moveg001115@users.noreply.github.com>
2023-09-03 16:38:22 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var answer = -1;
+    for(let i = 0; i < num_list.length; i++) {
+        if(num_list[i] < 0) {
+            answer = num_list.indexOf(num_list[i]);
+            break;
+        }
+    }
+    return answer;
+}
