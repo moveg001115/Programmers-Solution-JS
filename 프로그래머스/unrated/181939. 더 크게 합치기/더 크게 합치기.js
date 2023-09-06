485171597c1d4232ff38ab1f0d1b457f397d0e2d
moveg001115 <136950328+moveg001115@users.noreply.github.com>
2023-09-06 16:45:38 +0900

    [unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,4 +1,3 @@
 function solution(a, b) {
-    var answer = Number(String(a).concat(String(b))) > Number(String(b).concat(String(a))) ? Number(String(a).concat(String(b))) : Number(String(b).concat(String(a)));
-    return answer;
+    return Math.max(Number(`${a}${b}`), Number(`${b}${a}`))
 }
