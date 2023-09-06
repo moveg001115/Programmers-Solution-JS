1a20ec60ae27fcb47e89328d60824327a480360e
moveg001115 <136950328+moveg001115@users.noreply.github.com>
2023-09-06 17:07:43 +0900

    [unrated] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,9 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+    if(ineq === ">" && eq === "=") answer = n >= m ? 1 : 0;
+    else if(ineq === ">" && eq === "!") answer = n > m ? 1 : 0;
+    else if(ineq === "<" && eq === "=") answer = n <= m ? 1 : 0;
+    else answer = n < m ? 1 : 0;
+    
+    return answer;
+}
